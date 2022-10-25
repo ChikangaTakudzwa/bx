57db136e0f9d8c222e8a32e8280de9631c09f17f
ChikangaTakudzwa <takudzwachikanga@outlook.com>
2022-10-25 18:18:35 +0000

    Home page done for now

--- brandxpert/bx/views.py
+++ brandxpert/bx/views.py
@@ -5,4 +5,4 @@ def index(requests):
     context = {
         "title" : "Home | Brandxperts"
     }
-    return render(request, "base.html", context)
+    return render(requests, "index/index.html", context)
