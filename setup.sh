ade7b107e48abda33b60a34344aaef2f3c5fc744
ChikangaTakudzwa <69901316+ChikangaTakudzwa@users.noreply.github.com>
2022-11-19 22:00:09 +0000

    rm .bxenv

--- setup.sh
+++ setup.sh
@@ -11,14 +11,14 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.8 python3.8-venv
 
 echo "Creating a Python virtual environment"
-python3.8 -m venv .bxenv
+python3.8 -m venv bxenv
 
 echo "Checking the Python version..."
 python3 --version
 
 echo "Installing Python depenencies..."
-source .bxenv/bin/activate && python3 -m pip install --upgrade pip wheel
-source .bxenv/bin/activate && pip install -r ./brandxpert/requirements.txt
+source bxenv/bin/activate && python3 -m pip install --upgrade pip wheel
+source bxenv/bin/activate && pip install -r ./brandxpert/requirements.txt
 
 echo "****************************************"
 echo " Bx Environment Setup Complete"
