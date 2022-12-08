faade0ea2a31e027e970622e257aefc2a433528b
ChikangaTakudzwa <69901316+ChikangaTakudzwa@users.noreply.github.com>
2022-12-09 01:55:34 +0200

    Revert "Feat: Added scroll to top in footer and social media links"

--- brandxpert/bx/static/js/functions.js
+++ brandxpert/bx/static/js/functions.js
@@ -1,27 +1,11 @@
-jQuery(document).ready(function () {
-  $(window).scroll(function () {
-    $(".topnav").toggleClass(
-      "bg-white navbar-light shadow-sm scrollednav py-0",
-      $(this).scrollTop() > 50
-    );
-  });
+jQuery( document ).ready(function() {
+ 
+    $(window).scroll(function(){
+    $('.topnav').toggleClass('bg-white navbar-light shadow-sm scrollednav py-0', $(this).scrollTop() > 50);
+    });
 
-  $("#modal_newsletter").on("show.bs.modal", function () {
-    $(".downloadzip")[0].click();
-  });
-});
-
-$(document).ready(function () {
-  $("#back-to-top").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    return false;
-  });
-});
-
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 100) {
-    $("#back-to-top").fadeIn();
-  } else {
-    $("#back-to-top").fadeOut();
-  }
+    $('#modal_newsletter').on('show.bs.modal', function () {
+      $('.downloadzip')[0].click();
+    });
+    
 });
