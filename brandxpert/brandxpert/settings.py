79d8fcaf4fa09c7843dccf621f69f6aeb5896181
ChikangaTakudzwa <69901316+ChikangaTakudzwa@users.noreply.github.com>
2022-12-12 10:02:41 +0000

    Added condition on test db

--- brandxpert/brandxpert/settings.py
+++ brandxpert/brandxpert/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,20 +78,26 @@ WSGI_APPLICATION = 'brandxpert.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ChikangaTakudzwa/brandxpert',
-        'USER': 'ChikangaTakudzwa',
-        'PASSWORD': 'v2_3wchu_YSdDFEAZENdYUpXEDPDQBpr',
-        'HOST': 'db.bit.io',
-        'PORT': '5432',
-    },
-    'TEST': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        },
-}
+TESTING = sys.argv[1:2] == ['test']
+if TESTING == False:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'ChikangaTakudzwa/brandxpert',
+            'USER': 'ChikangaTakudzwa',
+            'PASSWORD': 'v2_3wchu_YSdDFEAZENdYUpXEDPDQBpr',
+            'HOST': 'db.bit.io',
+            'PORT': '5432',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+        "ENGINE": "django.db.backends.sqlite3",
+        "TEST": {
+            "NAME": os.path.join(BASE_DIR, "test_db.sqlite3"),
+        }
+    }}
 
 
 # Password validation
