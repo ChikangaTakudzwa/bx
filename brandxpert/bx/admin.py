76f48c712ec053307eecf875ba192933db26663e
ChikangaTakudzwa <69901316+ChikangaTakudzwa@users.noreply.github.com>
2022-12-12 14:09:45 +0000

    Added admin site- fix fed admin.py line error

--- brandxpert/bx/admin.py
+++ brandxpert/bx/admin.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
 from .models import WorksTable, MotionTable
 
+
 # Register your models here.
 @admin.register(WorksTable)
 class WorksAdmin(admin.ModelAdmin):
     """ WorksTable Admin Reg """
     list_display = ("file_name", "image", "serviceType", "date_uploaded")
 
-# admin.site.register()
 
 @admin.register(MotionTable)
 class MotionAdmin(admin.ModelAdmin):
     """ MotionTable Admin Reg """
     list_display = ("motion_name", "motion_link", "linkThumbnail", "date_uploaded")
-
-# admin.site.register(MotionTable)
