02b080ef41993eda6db2152c8926d8d3fb93faac
ChikangaTakudzwa <69901316+ChikangaTakudzwa@users.noreply.github.com>
2022-12-12 11:08:02 +0000

    Finish up portfolio2

--- brandxpert/bx/models.py
+++ brandxpert/bx/models.py
@@ -3,7 +3,9 @@ from datetime import datetime
 
 services = (("Logo", "logo"), ("Graphic Design", "graphics"), ("Info Graphics", "info"))
 
+
 class Meta:
+    """ Meta class for watching over the model.py """
     managed = True
 
 
@@ -14,7 +16,6 @@ class WorksTable(models.Model):
     image = models.ImageField(upload_to ='bx/media', help_text="Image to show")  # noqa: E251
     serviceType = models.CharField(max_length=50, choices=services, default=1, help_text="Service Type")
     date_uploaded = models.DateField(default=datetime.now, blank=True, help_text="Date uploaded")
-
 
     def __str__(self):
         """String for representing the Model object."""
@@ -27,7 +28,6 @@ class MotionTable(models.Model):
     linkThumbnail = models.ImageField(upload_to ='bx/media', help_text="Image")  # noqa: E251
     motion_link = models.URLField(max_length=200, help_text="Youtube Link")
     date_uploaded = models.DateField(default=datetime.now, blank=True, help_text="Date uploaded")
-
 
     def __str__(self):
         """String for representing the Model object."""
