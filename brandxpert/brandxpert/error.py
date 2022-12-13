eb8dbc2e3861e7253c97d58e649f670a062edca6
ChikangaTakudzwa <takudzwachikanga@outlook.com>
2022-12-13 17:50:00 +0000

    Finished error handling, fix flake err

--- brandxpert/brandxpert/error.py
+++ brandxpert/brandxpert/error.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 
 def page_not_found_view(request, exception):
     """ 404 handler view """
-    return render (request, "error/404.html", status=404)
+    return render(request, "error/404.html", status=404)
 
 # def page_not_found_view(request):
 #     """ 404 handler view """
 #     context = {
 #         "title": "Page Not Found - 404",
 #     }
-#     return render (request, "error/404.html", context)
+#     return render(request, "error/404.html", context)
