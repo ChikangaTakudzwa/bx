825e344a203c5a64851fd1230a5b1fab8e8f7a47
ChikangaTakudzwa <69901316+ChikangaTakudzwa@users.noreply.github.com>
2022-12-14 22:41:46 +0000

    Mod settings, json.loads

--- brandxpert/brandxpert/settings.py
+++ brandxpert/brandxpert/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,7 +35,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+hosts = env('ALLOWED_HOSTS')
+
+host = json.loads(hosts)
+
+ALLOWED_HOSTS = host
 
 MEDIA_ROOT = ''
 MEDIA_URL = ''
