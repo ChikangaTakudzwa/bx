4d8a17bab1ee3b19c3dd612550cf087cb65b423b
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-01-07 20:15:58 +0000

    Added all hosts for the main branch in the settings py file

--- brandxpert/brandxpert/settings.py
+++ brandxpert/brandxpert/settings.py
@@ -18,10 +18,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+env = environ.Env()
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
@@ -35,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".gitpod.io"]
+ALLOWED_HOSTS = [".gitpod.io", ".vercel.app", ".now.sh", "tornadozw.pythonanywhere.com", "localhost"]
 
 MEDIA_ROOT = ''
 MEDIA_URL = ''
