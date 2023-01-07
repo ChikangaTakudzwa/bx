0d2c039b7b32a6a84e6a05fed87113f33dcc455b
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-01-07 20:01:03 +0000

    Container running

--- brandxpert/brandxpert/settings.py
+++ brandxpert/brandxpert/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://*.gitpod.io" ]
+ALLOWED_HOSTS = [".gitpod.io"]
 
 MEDIA_ROOT = ''
 MEDIA_URL = ''
