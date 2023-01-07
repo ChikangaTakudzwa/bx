ab58699b90232c1064149861030f8ca4837207cb
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-01-07 22:42:47 +0000

    Container running

--- brandxpert/brandxpert/settings.py
+++ brandxpert/brandxpert/settings.py
@@ -18,10 +18,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+env = environ.Env()
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
@@ -35,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh", "tornadozw.pythonanywhere.com", "localhost"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh", "tornadozw.pythonanywhere.com", "localhost", ".gitpod.io"]
 
 MEDIA_ROOT = ''
 MEDIA_URL = ''
