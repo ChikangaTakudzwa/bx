b1d84da468c36f86662b670706f36c74e760f42c
ChikangaTakudzwa <69901316+ChikangaTakudzwa@users.noreply.github.com>
2023-01-09 10:34:17 +0000

    Added openshift host

--- brandxpert/brandxpert/settings.py
+++ brandxpert/brandxpert/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh", "tornadozw.pythonanywhere.com", "localhost", ".gitpod.io"]
+ALLOWED_HOSTS = [".openshiftapps.com"]
 
 MEDIA_ROOT = ''
 MEDIA_URL = ''
